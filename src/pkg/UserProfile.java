e938c20f3e6375a593cc44bf172e2278392b6532
Brett A Gloomis <gw1712@wayne.edu>
2021-03-08 19:21:05 -0500

    Sprint 1 Class files

--- src/pkg/UserProfile.java
+++ src/pkg/UserProfile.java
@@ -1,5 +1,25 @@
 package pkg;
 
 public class UserProfile {
+    String lastName;
+    String firstName;
+    String userID;
+    String password;
+    Roles userRole;
 
+    void login(){
+
+    }
+
+    void logout(){
+
+    }
+
+    void createPassword(){   //when user profile is first loggin
+
+    }
+
+    public UserProfile(){ //constructor
+
+    }
 }
