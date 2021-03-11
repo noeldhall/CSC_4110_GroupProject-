94fd63973114829969378a0045952589936d2635
Brett A Gloomis <gw1712@wayne.edu>
2021-03-10 19:40:32 -0500

    Sprint 1 Class files

--- src/pkg/Main.java
+++ src/pkg/Main.java
@@ -1,23 +1,60 @@
 package pkg;
 
-import java.util.Scanner;
+import java.awt.EventQueue;
 
-class Main{
-	/*
-	public static void main(String[] args){
-		---DONT DELETE---
-		 * This main will be a temporary testing ground for our objects. 
+import javax.swing.JFrame;
+
+public class Main {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Main() {
+		initialize();
+		Database db = new Database();
+		db.printVendors();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+/*
+        ---DONT DELETE---
+		 * This main will be a temporary testing ground for our objects.
 		 * Feel free to delete/ add test code at your leisure here
 		 * This main will not be included in the final project because we'll require GUI implementation
 		 * later down the road.
 		 * -nick
-		---END DONT DELETE--- 
-		
+		---END DONT DELETE---
+
 		Database db = new Database();
 		db.printVendors();
 	}
-	*/
-    /*
+
+
     UserProfile profile = new UserProfile("Nick", "Foster", "111111", "", Roles.OWNER);
     Scanner in = new Scanner(System.in);
     String username, password;
@@ -33,6 +70,5 @@ class Main{
 	    System.out.println("Good Job Sport!");
 
 	    in.close();
-
-     */
+*/
 }
