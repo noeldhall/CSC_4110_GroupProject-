cfa9fed065161be4416ca303edd00e0eed7ae6c7
Nicholas Foster <hb7798@wayne.edu>
2021-03-10 19:12:15 -0500

    testing GUI stuff

--- src/pkg/Main.java
+++ src/pkg/Main.java
@@ -1,18 +1,18 @@
 package pkg;
 
-import java.util.Vector;
-
 class Main{
+	/*
 	public static void main(String[] args){
-		/*---DONT DELETE---
+		---DONT DELETE---
 		 * This main will be a temporary testing ground for our objects. 
 		 * Feel free to delete/ add test code at your leisure here
 		 * This main will not be included in the final project because we'll require GUI implementation
 		 * later down the road.
 		 * -nick
-		---END DONT DELETE--- */
+		---END DONT DELETE--- 
 		
 		Database db = new Database();
 		db.printVendors();
 	}
+	*/
 }
