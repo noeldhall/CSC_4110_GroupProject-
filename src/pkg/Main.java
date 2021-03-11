d3a3a7baad78eb1ac1fdb06e17d19fdd016d1f85
Brett A Gloomis <gw1712@wayne.edu>
2021-03-10 19:21:14 -0500

    Sprint 1 Class files

--- src/pkg/Main.java
+++ src/pkg/Main.java
@@ -1,5 +1,7 @@
 package pkg;
 
+import java.util.Scanner;
+
 class Main{
 	/*
 	public static void main(String[] args){
@@ -15,4 +17,22 @@ class Main{
 		db.printVendors();
 	}
 	*/
+    /*
+    UserProfile profile = new UserProfile("Nick", "Foster", "111111", "", Roles.OWNER);
+    Scanner in = new Scanner(System.in);
+    String username, password;
+
+	    do {
+        System.out.println("enter username: ");
+        username = in.nextLine();
+
+        System.out.println("Enter password: ");
+        password = in.nextLine();
+    }while(!profile.login(username, password));
+
+	    System.out.println("Good Job Sport!");
+
+	    in.close();
+
+     */
 }
