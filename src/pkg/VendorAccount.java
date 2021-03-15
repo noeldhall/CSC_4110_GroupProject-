a800755d6f03e16576ea99f463722f1e15ae29dd
Nicholas Alexander Francis Eric Foster <hb7798@wayne.edu>
2021-03-15 16:47:26 -0400

    Update VendorAccount.java

--- src/pkg/VendorAccount.java
+++ src/pkg/VendorAccount.java
@@ -1,8 +1,13 @@
 package pkg;
 
-public class VendorAccount {
+public class VendorAccount implements Comparable<VendorAccount>{
 	double balance;
 	double lastPaidAmount;
+	
+	public VendorAccount() {
+		balance = 0.00;
+		lastPaidAmount = 0.00;
+	}
 	
 	public VendorAccount(double balance, double lastPaidAmount) {
 		this.balance = balance;
@@ -11,8 +16,33 @@ public class VendorAccount {
 	
 	public String toString() {
 		String data = "";
-		data = "balance: " + balance + "\n" + 
-				"last Paid Amount: " + lastPaidAmount + "\n";
+		data = "balance: " + balance + "\t" + 
+				"last Paid Amount: " + lastPaidAmount + "\t";
 		return data;
+	}
+	
+	public int compareTo(VendorAccount o) {
+		if(balance == o.balance) {
+			return 0;
+		}
+		else {
+			return (int) (balance - o.balance);
+		}
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public double getLastPaidAmount() {
+		return lastPaidAmount;
+	}
+
+	public void setLastPaidAmount(double lastPaidAmount) {
+		this.lastPaidAmount = lastPaidAmount;
 	}
 }
