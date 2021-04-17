8cec1ea594210bbd18dd6a7cf6e138a82dd962be
Nicholas Foster <hb7798@wayne.edu>
2021-04-17 00:18:28 -0400

    updated readme, made a purchaser gui class

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Authors: Brett Gloomis, Nicholas Foster, Noel Hall, Tyler Groat
+
+Necessities for compiliation:
+	- ensure eclipse run configurations sets the project folder as the target folder
+		1. go to run > run configurations
---
+++ src/guiPkg/PurchaserGUI.java
@@ -0,0 +1,14 @@
+package guiPkg;
+
+import javax.swing.JPanel;
+
+public class PurchaserGUI extends JPanel {
+	public PurchaserGUI() {
+	}
+
+	/**
+	 * Author: Nicholas Foster
+	 */
+	private static final long serialVersionUID = 3360425635918542473L;
+	
+}
